326f4b1d62836d9008ae4fda362cebfde482c433
Lee Faus <lee.faus@armory.io>
2020-08-15 22:59:12 -0400

    moving constant out

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ async function run(template_owner, template_repo, pilot) {
     // The YML workflow will need to set myToken with the GitHub Secret Token
     // myToken: ${{ secrets.GITHUB_TOKEN }}
     // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    const ghToken = core.getInput('token');
 
-    const octokit = github.getOctokit(ghToken)
 
     // You can also pass in additional options as a second parameter to getOctokit
     // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
@@ -26,6 +24,8 @@ try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('name');
     console.log(`Hello ${nameToGreet}!`);
+    const ghToken = core.getInput('token');
+    const octokit = github.getOctokit(ghToken)
     // Get the JSON webhook payload for the event that triggered the workflow
     // const payload = JSON.stringify(github.context.payload, undefined, 2)
     // console.log(`The event payload: ${payload}`);
